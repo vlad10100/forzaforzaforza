74a8eacdf93420399c3f84c614c3a9decff73c62
vlad10100 <vladimircaralde@gmail.com>
2024-02-07 01:33:52 +0800

    ft: merch detail

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,33 +1,64 @@
 <template>
   <Page>
-    <div class="h-screen">
-      <p>
-        {{ sport }}
-      </p>
-      <p>{{ category }}</p>
-      <p>{{ productType }}</p>
-      <p>{{ itemId }}</p>
+    <div class="h-screen flex justify-center mt-20">
+      <div>
+        <div class="flex gap-5 mb-10">
+          <IcChevron direction="left" @click="goBack" />
+          <p>{{ merch?.category }} (create Breadcrumbs)</p>
+        </div>
+
+        <div>
+          <p>product name: {{ merch?.label }}</p>
+        </div>
+
+        <div>price: {{ merch?.price }}</div>
+        <div>merch id: {{ merch?.id }}</div>
+
+        <div class="w-full flex justify-center">
+          <img :src="`/singlets/${merch?.link}`" />
+        </div>
+        <div>
+          <p>Buy Now</p>
+          <p>Add to Cart</p>
+        </div>
+      </div>
     </div>
   </Page>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import Page from './layout/Page.vue'
+import { useMerchendiseStore } from '@/stores/shop'
+import IcChevron from '@/components/icons/IcChevron.vue'
+
+type Merchendise = {
+  id: Number
+  category: String
+  label: String
+  link: String
+  price: Number
+}
+
+const myStore = useMerchendiseStore()
 
 const route = useRoute()
-const sport = ref<string>('')
-const category = ref<string>('')
-const productType = ref<string>('')
+const router = useRouter()
 const itemId = ref<number>(0)
+const merch = ref<Merchendise | null>(null)
 
 onMounted(() => {
-  sport.value = route.params.sport as string
-  productType.value = route.params.productType as string
-  category.value = route.params.category as string
   itemId.value = parseInt(route.params.id as string)
+
+  const data = myStore.getMerchById(itemId.value)
+  if (data) merch.value = data
 })
+
+const goBack = () => {
+  console.log('vlad')
+  router.go(-1)
+}
 </script>
 
 <style scoped></style>
