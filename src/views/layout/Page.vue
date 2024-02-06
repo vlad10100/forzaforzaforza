bef0bc1c356ce7c235099118a037bf450a014a52
vlad10100 <vladimircaralde@gmail.com>
2024-02-07 01:01:47 +0800

    chore: scroll to top

--- src/views/layout/Page.vue
+++ src/views/layout/Page.vue
@@ -7,6 +7,10 @@
 
 <script setup lang="ts">
 import Footer from '../../components/common/Footer.vue'
+
+// Scroll to top
+document.body.scrollTop = 0
+document.documentElement.scrollTop = 0
 </script>
 
 <style scoped></style>
