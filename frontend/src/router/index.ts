23e92f4ede93841d5807ebed0942c6d897e222b9
vlad10100 <vladimircaralde@gmail.com>
2024-02-21 15:59:39 +0800

    fix: router history

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory('/forza/'), //'/forza/'
+  history: createWebHistory('/beta/'),
   routes: [
     {
       name: 'home',
