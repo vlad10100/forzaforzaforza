2ecf30964a4388e37fe85baf09f990a1c575b05a
vlad10100 <vladimircaralde@gmail.com>
2024-02-22 10:04:40 +0800

    ft: base button

---
+++ frontend/src/components/buttons/BaseButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="$emit('click')" class="flex justify-center items-center">
+    <slot name="left-icon"></slot>
+    <p>
+      {{ label }}
+    </p>
+    <slot name="right-icon"></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['click'])
+defineProps({
+  label: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style scoped></style>
