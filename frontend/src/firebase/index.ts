001ab1bc56936b2dac96a68572d30088a5cdaff0
vlad10100 <vladimircaralde@gmail.com>
2024-02-24 15:20:39 +0800

    fix: use firebase auth

--- frontend/src/firebase/index.ts
+++ frontend/src/firebase/index.ts
@@ -8,7 +8,7 @@ import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDuAdF7JIjSCQvJFVhu4KpbaRO2fp-BORE',
-  authDomain: 'auth.forzaforzaforza.com',
+  authDomain: 'forzaforzaforza-3fb61.firebaseapp.com',
   projectId: 'forzaforzaforza-3fb61',
   storageBucket: 'forzaforzaforza-3fb61.appspot.com',
   messagingSenderId: '144609460325',
