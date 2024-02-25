0fb3f006957b92f6e994bef91f0bf65793676a39
vlad10100 <vladimircaralde@gmail.com>
2024-02-25 22:07:42 +0800

    ft: activity card

--- frontend/src/views/app/AllActivities.vue
+++ frontend/src/views/app/AllActivities.vue
@@ -1,17 +1,31 @@
 <template>
   <div>
-    <div class="text-3xl" v-if="!isLoading">All Activities</div>
-    <div class="py-10" v-for="(activity, index) in activities" :key="index">
-      <div class="flex justify-center">
-        <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
-          <path :d="activity.svg_path" fill="none" stroke="#e85d04" stroke-width="3"></path>
-        </svg>
+    <div class="text-3xl my-10" v-if="!isLoading">All Activities</div>
+    <div class="space-y-10">
+      <div class="" v-for="(activity, index) in activities" :key="index">
+        <div class="border rounded-lg shadow-md max-w-[600px] w-full divide-y divide-gray-300">
+          <div class="flex justify-center md:px-10 px-5 md:py-5 py-2 bg-gray-50">
+            <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
+              <path :d="activity.svg_path" fill="none" stroke="#e85d04" stroke-width="3"></path>
+            </svg>
+          </div>
+          <div class="text-left md:px-10 px-2 md:py-5 py-2">
+            <div class="my-2">
+              <p class="md:text-xs text-[11px]">{{ activity.parsed_date }}</p>
+              <p class="text-lg">{{ activity.name }}</p>
+            </div>
+            <div class="md:text-sm text-xs">
+              <p>Distance: {{ (activity.distance / 1000).toFixed(2) }} kilometers</p>
+              <p>Avg. pace: {{ activity.average_pace }}</p>
+              <p>
+                Moving time: <span class="truncate">{{ activity.parsed_moving_time }}</span>
+              </p>
+              <p>Avg. heartrate: {{ activity.average_heartrate }}bpm</p>
+              <p>Max heartrate: {{ activity.max_heartrate }}bpm</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <p>Distance: {{ (activity.distance / 1000).toFixed(2) }}</p>
-      <p>Type: {{ activity.sport_type }}</p>
-      <p>Name: {{ activity.name }}</p>
-      <p>Max HR: {{ activity.max_heartrate }}</p>
-      <p>Date: {{ activity.start_date_local }}</p>
     </div>
   </div>
 </template>
@@ -21,10 +35,13 @@ type Activity = {
   id: Number
   name: String
   sport_type: String
-  start_date_local: Date
+  parsed_date: String
   max_heartrate: Number
   svg_path: string
   distance: number
+  average_heartrate: Number
+  average_pace: string
+  parsed_moving_time: string
 }
 
 defineProps({
