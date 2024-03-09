e0b4d844958e59bb3cc14e908bb028e12cc77229
vlad10100 <vladimircaralde@gmail.com>
2024-03-09 19:35:04 +0800

    fix: commonstore

--- frontend/src/stores/common/index.ts
+++ frontend/src/stores/common/index.ts
@@ -1,23 +1,8 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
-import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from '@/firebase'
 
 export const useCommonStore = defineStore('commonStore', () => {
-  const isLoading = ref(false)
-  const isFetchingUser = ref(false)
+  const loadingWholePage = ref(false)
 
-  const signedInUser = ref()
-
-  const loadSignedInUser = async () => {
-    if (!signedInUser.value) {
-      onAuthStateChanged(auth, async (user) => {
-        signedInUser.value = user
-        return signedInUser.value
-      })
-    }
-    return signedInUser.value
-  }
-
-  return { isLoading, isFetchingUser, signedInUser, loadSignedInUser }
+  return { loadingWholePage }
 })
