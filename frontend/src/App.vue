a019e684a7dc6812cb8d9c89647fd64c35b56a79
vlad10100 <vladimircaralde@gmail.com>
2024-03-09 19:19:20 +0800

    ft: refactor and simplify app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,19 @@
 <template>
   <div class="overflow-hidden relative">
-    <NavBar ref="navBar" />
-
-    <div class="overflow-y-auto scrollbar fixed w-full top-16 navbarHeight">
+    <NavBar />
+    <div class="overflow-y-auto scrollbar fixed w-full top-16 main-container">
       <RouterView />
-      <div class="fixed inset-0 bg-white z-50 bg-opacity-75 top-16" v-if="commonStore.isLoading">
-        <div class="h-full flex items-center justify-center">
-          <Loader size="200" />
-        </div>
-      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { useCommonStore } from './stores/common'
-import NavBar from './components/common/Navbar.vue'
-import Loader from './components/common/Loader.vue'
-import { getSignedInUser } from '@/composables'
-import { useAthleteStore } from './stores/athlete'
-
-const navBar = ref(null)
-const commonStore = useCommonStore()
-const athleteStore = useAthleteStore()
-
-onMounted(async () => {
-  commonStore.isFetchingUser = true
-  const user = await getSignedInUser()
-  if (!user) {
-    commonStore.isFetchingUser = false
-    commonStore.isLoading = false
-    return
-  }
-  commonStore.signedInUser = user
-  const athlete = await athleteStore.loadAthlete(commonStore.signedInUser.uid)
-  commonStore.signedInUser['connected_to_strava'] = athlete?.connected_to_strava || false
-  commonStore.isFetchingUser = false
-})
+import NavBar from '@/components/common/Navbar.vue'
 </script>
 
 <style scoped>
-.navbarHeight {
+.main-container {
   height: calc(100vh - 64px);
 }
 </style>
