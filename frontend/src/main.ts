dca4b646c93edd68d26cce85f5db93d6a6370508
vlad10100 <vladimircaralde@gmail.com>
2024-03-09 19:32:11 +0800

    ft: main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,21 @@
-import './assets/style/main.css'
+import '@/assets/style/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import vue3GoogleLogin from 'vue3-google-login'
 
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+
+import router from '@/router'
+import axios from '@/core/axios'
 
 const app = createApp(App)
 
+const CLIENT_ID = '144609460325-khh5h8h6erb15v3rjpjbcrh8qheg8sv2.apps.googleusercontent.com'
+
+app.use(axios)
+
+app.use(vue3GoogleLogin, { clientId: CLIENT_ID })
 app.use(createPinia())
 app.use(router)
 
