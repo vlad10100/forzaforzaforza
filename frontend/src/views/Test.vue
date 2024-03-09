be2e39b43801a204b9b4e7fc0bf55aa9b913bb8b
vlad10100 <vladimircaralde@gmail.com>
2024-03-09 19:34:38 +0800

    test: google login

--- frontend/src/views/Test.vue
+++ frontend/src/views/Test.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>test</div>
+  <div>
+    <GoogleLogin
+      :callback="callback"
+      prompt
+      auto-login
+    ></GoogleLogin>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { GoogleLogin, decodeCredential } from 'vue3-google-login'
+
+type Response = {
+  clientId: string
+  client_id: string
+  credential: string
+  select_by: string
+}
+
+const callback = async (response: Response) => {
+  console.log('Handle the response', decodeCredential(response.credential))
+}
+</script>
 
 <style scoped></style>
