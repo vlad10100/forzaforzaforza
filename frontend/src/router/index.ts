d720dd62afbf56b12b012da6121aada9e18ef64b
vlad10100 <vladimircaralde@gmail.com>
2024-03-09 19:16:47 +0800

    fix: router and router guards

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { useCommonStore } from '@/stores/common'
-import { getSignedInUser } from '@/composables'
+import { USER_KEY } from '@/stores/user'
 
 const router = createRouter({
   history: createWebHistory('/beta/'),
@@ -8,85 +7,89 @@ const router = createRouter({
     {
       name: 'home',
       path: '/',
-      component: () => import('@/views/Home.vue')
+      component: () => import('@/views/Home.vue'),
     },
     {
       name: 'run',
       path: '/run',
       component: () => import('@/views/app/Run.vue'),
-      meta: { requiresAuth: true }
+      meta: { requiresAuth: true, requiresProfile: true },
     },
     {
       name: 'calendar',
       path: '/calendar',
-      component: () => import('@/views/Calendar.vue')
+      component: () => import('@/views/Calendar.vue'),
     },
     {
       name: 'sale',
       path: '/sale',
-      component: () => import('@/views/Sale.vue')
+      component: () => import('@/views/Sale.vue'),
     },
     {
       name: 'about-us',
       path: '/about-us',
-      component: () => import('@/views/AboutUs.vue')
+      component: () => import('@/views/AboutUs.vue'),
     },
     {
-      name: 'sign-in',
-      path: '/sign-in',
-      component: () => import('@/views/auth/SignIn.vue')
+      name: 'auth',
+      path: '/auth',
+      component: () => import('@/views/auth/Auth.vue'),
     },
     {
       name: 'register',
       path: '/register',
-      component: () => import('@/views/auth/Register.vue')
+      component: () => import('@/views/auth/Register.vue'),
     },
     {
       name: 'shop',
       path: `/shop/:sport/:productType/:category/:id`,
-      component: () => import('@/views/Shop.vue')
+      component: () => import('@/views/Shop.vue'),
     },
     {
       name: 'profile',
       path: '/profile',
       component: () => import('@/views/Profile.vue'),
-      meta: { requiresAuth: true }
+      meta: { requiresAuth: true },
     },
     {
       name: 'test',
       path: '/test',
-      component: () => import('@/views/Test.vue')
-    },
-    {
-      name: 'vertical',
-      path: '/vertical',
-      component: () => import('@/views/Vertical.vue')
+      component: () => import('@/views/Test.vue'),
     },
     {
       name: 'forza-strava-auth',
       path: '/forza-strava-auth',
-      component: () => import('@/views/auth/ForzaStravaAuth.vue')
+      component: () => import('@/views/auth/ForzaStravaAuth.vue'),
     },
-    { path: '/:pathMatch(.*)*', name: 'NotFound', component: () => import('@/views/NotFound.vue') }
-  ]
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: () => import('@/views/NotFound.vue'),
+    },
+  ],
 })
 
 // Auth guard
-router.beforeEach(async (to, from, next) => {
-  const user = await getSignedInUser()
+router.beforeEach(async (to, _from, next) => {
+  const user = JSON.parse(localStorage.getItem(USER_KEY) as string)
 
-  if (to.matched.some((record) => record.meta.requiresAuth)) {
-    if (!user) {
-      console.log('UNAUTH')
+  // If the route requires auth and the user is not logged in, redirect to /auth
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!user || !user.email || !user.user_id) {
       next({
-        path: '/'
+        path: '/auth',
       })
-    } else {
-      next()
     }
-  } else {
-    next()
   }
-})
 
+  // If the route requires a profile and the user has not set one up, redirect to /profile
+  if (to.matched.some(record => record.meta.requiresProfile)) {
+    if (!user.username) {
+      next({
+        path: '/profile',
+      })
+    }
+  }
+  next()
+})
 export default router
